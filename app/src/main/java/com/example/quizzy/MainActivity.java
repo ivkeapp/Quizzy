83256ff40e2e0440569ffaf4682a3a56e0376085
Zarkovic <ivanzarkovic@yahoo.com>
2020-03-17 16:37:41 +0100

    add memeber variables

--- app/src/main/java/com/example/quizzy/MainActivity.java
+++ app/src/main/java/com/example/quizzy/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.quizzy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TODO: Declare constant variables
+
+    //TODO: Declare member variables
+    Button mTrueButton;
+    Button mFalseButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_btn);
+        mFalseButton = findViewById(R.id.false_btn);
     }
 }
